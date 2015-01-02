7ad2e319f291796ebf07587e53a4c13ed0a58c75
Ian Hansen <ian@supershabam.com>
2015-01-02 13:45:05 -0700

    purging the old, ghetto testing the new

--- cmd/iptisch/main.go
+++ cmd/iptisch/main.go
@@ -2,51 +2,28 @@ package main
 
 import (
 	"flag"
-	"io/ioutil"
 	"log"
-	"os/exec"
-	"strings"
-	"time"
 
 	"github.com/supershabam/iptisch"
 )
 
-// failure: database not available
-// - avoid trampling herd
-// - last successful run should be loaded in iptables
-//
-// failure: generated firewall rules are invalid
-// - log failure, do not save file
-//
-
 var (
-	template = flag.String("template", "", "template to read")
+	servers = flag.String("servers", "", "comma separated list of zookeeper addresses")
 )
 
 func main() {
 	flag.Parse()
-	templateData, err := ioutil.ReadFile(*template)
-	if err != nil {
+
+	gw := iptisch.ZKGroupWatcher{
+		Conn:  conn,
+		Group: "test",
+	}
+
+	for group := range gw.Watch() {
+		fmt.Printf("%+v\n", group)
+	}
+
+	if gw.Err() != nil {
 		log.Fatal(err)
-	}
-	t := iptisch.Template{
-		Text: string(templateData),
-	}
-	w := iptisch.Watcher{
-		Keys:   t.Keys(),
-		Period: time.Second * 2,
-	}
-	for variables := range w.Watch() {
-		cmd := exec.Command("iptables-restore")
-		cmd.Stdin = strings.NewReader(t.Execute(variables))
-		out, err := cmd.CombinedOutput()
-		if err != nil {
-			log.Printf("%s\n", out)
-			log.Fatal(err)
-		}
-		log.Printf("wrote rules to iptables")
-	}
-	if w.Err != nil {
-		log.Fatal(w.Err)
 	}
 }
