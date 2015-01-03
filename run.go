6221f1e04f724a444385d37edbd0350948599e1b
Ian Hansen <ian@supershabam.com>
2015-01-02 17:16:30 -0700

    sort so that dedupe works

--- run.go
+++ run.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/samuel/go-zookeeper/zk"
 	"io/ioutil"
+	"sort"
 	"strings"
 )
 
@@ -29,7 +30,9 @@ func dedupe(input []string) (output []string) {
 func ipMap(input map[string][]string) (output map[string][]string) {
 	output = map[string][]string{}
 	for group, values := range input {
-		output[group] = dedupe(deSuffix(values))
+		values = dedupe(deSuffix(values))
+		sort.Strings(values)
+		output[group] = values
 	}
 	return
 }
@@ -51,8 +54,15 @@ func Run(conn *zk.Conn, root, path string) error {
 	gw := GroupsWatcher{
 		Watchers: watchers,
 	}
+	last := ""
 	for m := range gw.Watch() {
-		fmt.Println(t.Execute(ipMap(m)))
+		next := t.Execute(ipMap(m))
+		// dedupe if we generate the same result
+		if next == last {
+			continue
+		}
+		last = next
+		fmt.Println(next)
 	}
 	if err := gw.Err(); err != nil {
 		return err
