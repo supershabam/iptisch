a6765fe6948e2edc4a4420db76245ffb8b75ed3c
Ian Hansen <ian@supershabam.com>
2015-01-02 17:10:26 -0700

    dedupes ips and removes suffix

--- run.go
+++ run.go
@@ -4,7 +4,35 @@ import (
 	"fmt"
 	"github.com/samuel/go-zookeeper/zk"
 	"io/ioutil"
+	"strings"
 )
+
+func deSuffix(input []string) (output []string) {
+	for _, i := range input {
+		output = append(output, strings.Split(i, "-")[0])
+	}
+	return
+}
+
+func dedupe(input []string) (output []string) {
+	seen := map[string]bool{}
+	for _, i := range input {
+		if seen[i] {
+			continue
+		}
+		seen[i] = true
+		output = append(output, i)
+	}
+	return
+}
+
+func ipMap(input map[string][]string) (output map[string][]string) {
+	output = map[string][]string{}
+	for group, values := range input {
+		output[group] = dedupe(deSuffix(values))
+	}
+	return
+}
 
 func Run(conn *zk.Conn, root, path string) error {
 	raw, err := ioutil.ReadFile(path)
@@ -24,7 +52,7 @@ func Run(conn *zk.Conn, root, path string) error {
 		Watchers: watchers,
 	}
 	for m := range gw.Watch() {
-		fmt.Println(t.Execute(m))
+		fmt.Println(t.Execute(ipMap(m)))
 	}
 	if err := gw.Err(); err != nil {
 		return err
